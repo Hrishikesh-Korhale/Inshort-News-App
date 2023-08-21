eb562be018d70d00dda8a8027f663b9983287a80
Hrishi <hrishikorhale2002@gmail.com>
2023-08-21 13:29:09 +0530

    Add favicon icon & technology news  data

--- server/constants/technologydata.js
+++ server/constants/technologydata.js
@@ -5,7 +5,7 @@ export const technologydata = [
         author: 'Mansi Agarwal',
         description: 'The British government will reportedly spend £100 million to procure computer chips that can power AI systems. As per media reports, government officials are in talks with NVIDIA, AMD and Intel about buying equipment for national AI research. Authorities are in the advanced stages of buying up to 5,000 graphics processing units from NVIDIA, the reports stated.',
         url: 'https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2023/08_aug/20_sun/img_1692551444619_936.jpg?',
-        timestamp: '',
+        timestamp: '2023-08-24T07:54:25.000Z',
         link: 'https://tech.hindustantimes.com/amp/tech/news/uk-is-in-talks-with-tech-giants-on-ai-chips-telegraph-reports-71692531760226.html?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts',
         publisher: 'Hindustan Times'
     },
@@ -14,8 +14,8 @@ export const technologydata = [
         author: 'Nidhi Sinha',
         description: 'X (formerly Twitter) owner Elon Musk, whos being criticised for his decision to remove the ability to block others on the platform, admitted that he himself is currently using the block feature. Musk tweeted, Pretty fun blocking people who complain that blocking is going away. How does the medicine taste? An X user replied, Its...not funny. We block for safety.',
         url: 'https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2023/08_aug/21_mon/img_1692587175959_172.jpg?',
-        timestamp: 'https://twitter.com/elonmusk/status/1693219734001500381?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts',
-        link: '',
+        timestamp: '2023-08-24T07:54:25.000Z',
+        link: 'https://twitter.com/elonmusk/status/1693219734001500381?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts',
         publisher: 'Twitter'
     },
     {
@@ -23,7 +23,7 @@ export const technologydata = [
         author: 'Mansi Agarwal',
         description: 'Intel will lay off at least 140 employees in the US to reduce costs, as per a report by Sacramento Inno. The chipmaker will reportedly fire 89 employees from its Folsom R&D campus and 51 based in San Jose, California. In May, Intel announced its plans to cut some jobs after reporting its largest-ever quarterly loss.',
         url: 'https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2023/08_aug/20_sun/img_1692534898458_809.jpg?',
-        timestamp: '',
+        timestamp: '2023-08-24T07:54:25.000Z',
         link: 'https://www.latestly.com/technology/intel-layoffs-continue-as-chip-maker-cuts-140-jobs-in-us-to-reduce-costs-5350971.html?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts',
         publisher: 'LatestLY'
     },
@@ -32,7 +32,7 @@ export const technologydata = [
         author: 'Mansi Agarwal',
         description: 'X CEO Linda Yaccarino said, \"There is an important place in the world for X.\" She was commenting on X owner Elon Musks remark that its a \"sad truth\" that \"there are no great social networks right now\". \"We may fail, as so many have predicted, but well try our best to make there be at least one,\" he added.',
         url: 'https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2023/08_aug/20_sun/img_1692538741415_204.jpg?',
-        timestamp: '',
+        timestamp: '2023-08-24T07:54:25.000Z',
         link: 'https://twitter.com/lindayaX/status/1693043203933413701?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts',
         publisher: 'Twitter'
     },
@@ -41,7 +41,7 @@ export const technologydata = [
         author: 'Mansi Agarwal',
         description: 'Delhi High Court has dismissed two public interest litigations that sought Google Pay to cease operations in India over violations of regulatory and privacy norms. The petitions accused Google Pay of operating as a \"payment system provider\" and collecting users Aadhaar details without securing necessary permissions. The court, however, found no merit in the petitions.',
         url: 'https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2023/08_aug/20_sun/img_1692529723917_740.jpg?',
-        timestamp: '',
+        timestamp: '2023-08-24T07:54:25.000Z',
         link: 'https://yourstory.com/2023/08/delhi-high-court-dismisses-pils-google-pay?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts',
         publisher: 'YourStory'
     },
@@ -50,7 +50,7 @@ export const technologydata = [
         author: 'Pragya Swastik',
         description: 'Actor James Woods took to X (formerly Twitter) to criticise the platform and its owner Elon Musk for removing the ability to block others. \"Musk, whom I once championed, is only doing this to protect his advertisers anyway...The man I thought was a defender of free speech is just another greedy capitalist,\" Woods tweeted. \"Then delete your account,\" Musk responded.',
         url: 'https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2023/08_aug/20_sun/img_1692531864223_655.jpg?',
-        timestamp: '',
+        timestamp: '2023-08-24T07:54:25.000Z',
         link: 'https://twitter.com/elonmusk/status/1693216678975287615?s=20&utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts',
         publisher: 'Twitter'
     }
